ef4b9a8604bda24365b0a8165166bdf780509501
Daniel Reuther <d.reuther@hotmail.de>
2023-12-17 22:06:45 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { onMount } from "svelte";
+
+
+    let date = getNewDate();
+
+    function getNewDate() {
+        return new Date().toLocaleString().split(", ")[1];
+    }
+
+    onMount(() => {
+        const interval = setInterval(() => {
+            date = getNewDate();
+        }, 1000);
+
+        return () => {
+            clearInterval(interval);            
+        }
+    });
+
+</script>
+
+<div class="flex flex-col justify-center w-full items-center min-h-screen">
+    <div class="mb-5">
+        <span class="text-[55px]">Current time</span>
+    </div>
+    <div>
+        <span class="text-[45px]">{date}</span>
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: #8697d1;
+    }
+</style>
